0f0c096e7d46fb54f3945d732883ab87a5597dbe
Jay Shah <jayshah5109@gmail.com>
2019-08-20 10:58:51 +0530

    Revert "added Hello"
    This reverts commit fb5a4c033bea5661290a8fe7b141078ddb7b4a7f.

--- Sample.java
+++ Sample.java
@@ -2,6 +2,15 @@ public class Sample{
 public static void main(String args[])
 {
 System.out.println("Hello World!");
-System.out.println("Hello!");
+<<<<<<< HEAD
+<<<<<<< HEAD
+System.out.println("Have a nice day!");
+System.out.println("Good Morning!");
+
+=======
+>>>>>>> parent of a25baec... added Good Morning
+=======
+System.out.println("Good Morning!");
+>>>>>>> parent of 23b49ed... added have a nice day
 }
 }
