23b49ed372fd2197f92cb324f50dc930000dbde0
Jay Shah <jayshah5109@gmail.com>
2019-08-20 10:38:31 +0530

    added have a nice day

--- Sample.java
+++ Sample.java
@@ -2,6 +2,8 @@ public class Sample{
 public static void main(String args[])
 {
 System.out.println("Hello World!");
+System.out.println("Have a nice day!");
 System.out.println("Good Morning!");
+
 }
 }
