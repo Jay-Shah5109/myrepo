fb5a4c033bea5661290a8fe7b141078ddb7b4a7f
Jay Shah <jayshah5109@gmail.com>
2019-08-20 10:58:25 +0530

    added Hello

--- Sample.java
+++ Sample.java
@@ -2,15 +2,6 @@ public class Sample{
 public static void main(String args[])
 {
 System.out.println("Hello World!");
-<<<<<<< HEAD
-<<<<<<< HEAD
-System.out.println("Have a nice day!");
-System.out.println("Good Morning!");
-
-=======
->>>>>>> parent of a25baec... added Good Morning
-=======
-System.out.println("Good Morning!");
->>>>>>> parent of 23b49ed... added have a nice day
+System.out.println("Hello!");
 }
 }
